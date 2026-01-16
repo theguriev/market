6be475224c01a76f06b15b15f67ef59464808105
Eugen Guriev <gurievcreative@gmail.com>
2026-01-16 20:22:56 +0200

    feat: replace TeamSwitcher with Logo in AppSidebar and update layout

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -12,15 +12,23 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
+  SidebarMenuButton,
   SidebarRail,
 } from "@/components/ui/sidebar";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Logo } from "./ui/logo";
+import Link from "next/link";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>
-        <TeamSwitcher teams={sidebarData.teams} />
+      <SidebarHeader className="flex flex-row">
+        <SidebarMenuButton asChild tooltip="Головна">
+          <Link href="/">
+            <Logo className="size-5" />
+            <span className="group-data-[collapsible=icon]:hidden">Creotik</span>
+          </Link>
+        </SidebarMenuButton>
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={sidebarData.navMain} />
