b852eb2f3c1c1010f05ec3954cbd1cf34e879f5f
Eugen Guriev <gurievcreative@gmail.com>
2026-02-14 21:32:04 +0200

    feat: enhance CampaignCards component with improved layout and content presentation

--- app/components/cards.tsx
+++ app/components/cards.tsx
@@ -91,23 +91,27 @@ export function CampaignCards({
                 <div className="text-xs text-muted-foreground">
                   17 годин тому
                 </div>
-                <div className="mt-1 flex items-center gap-2 text-muted-foreground">
-                  <IconBrandInstagram
-                    className="size-4"
-                    aria-label="Instagram"
-                  />
-                  <IconBrandX className="size-4" aria-label="X" />
-                </div>
               </div>
-              <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-                $1000.00
-              </CardTitle>
             </div>
           </div>
         </CardHeader>
+        <CardContent>
+          <span>$1,000,000 Giveaway Stream</span>
+        </CardContent>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="text-muted-foreground">
-            $1,000,000 Giveaway Stream
+          <div className="flex justify-between w-full items-end">
+            <div className="mt-1 flex items-center gap-2 text-muted-foreground">
+              <IconBrandInstagram className="size-4" aria-label="Instagram" />
+              <IconBrandX className="size-4" aria-label="X" />
+            </div>
+            <div className="flex flex-col items-end">
+              <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                $1000.00
+              </CardTitle>
+              <span className="text-xs text-muted-foreground leading-none">
+                за 1М переглядів
+              </span>
+            </div>
           </div>
         </CardFooter>
       </Card>
@@ -129,20 +133,27 @@ export function CampaignCards({
                 <div className="text-xs text-muted-foreground">
                   17 годин тому
                 </div>
-                <div className="mt-1 flex items-center gap-2 text-muted-foreground">
-                  <IconBrandYoutube className="size-4" aria-label="YouTube" />
-                  <IconBrandTiktok className="size-4" aria-label="TikTok" />
-                </div>
               </div>
-              <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-                $2000.00
-              </CardTitle>
             </div>
           </div>
         </CardHeader>
+        <CardContent>
+          <span>Gaming Clips | I Caught a Cursed Bloop</span>
+        </CardContent>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="text-muted-foreground">
-            Gaming Clips | I Caught a Cursed Bloop
+          <div className="flex justify-between w-full items-end">
+            <div className="mt-1 flex items-center gap-2 text-muted-foreground">
+              <IconBrandYoutube className="size-4" aria-label="YouTube" />
+              <IconBrandTiktok className="size-4" aria-label="TikTok" />
+            </div>
+            <div className="flex flex-col items-end">
+              <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                $2000.00
+              </CardTitle>
+              <span className="text-xs text-muted-foreground leading-none">
+                за 1М переглядів
+              </span>
+            </div>
           </div>
         </CardFooter>
       </Card>
